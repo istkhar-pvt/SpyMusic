d46380b232db44b2786577f3cd23282a4f2cc1bc
KingOfMusicBot <teamriya84@gmail.com>
2025-12-04 22:11:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Iamistkhar")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/https://t.me/+xfr6-ZOTaZVmODU1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+xfr6-ZOTaZVmODU1")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
