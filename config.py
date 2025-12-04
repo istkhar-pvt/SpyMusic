6707d896346cdb06c4f58c04e96c64b0687ddd18
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2025-12-04 22:30:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots4556e3') # youtube song api ke # -------------------------------------------------------------------------#
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/istkhar-pvt/SpyMusic",
